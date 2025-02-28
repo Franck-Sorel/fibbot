e3eb2fd7e994b7dcd11f7f12c43614daea378cbb
Franck-Sorel <mfrancksorel@gmail.com>
2025-02-28 16:02:05 +0100

    Update: remove arguments

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,10 @@
-use std::env;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
 
-
-
-    // Check if the amount of arguments is exactly 3 (program name + 2 arguments)
-    // if args.len() != 3 {
-    //     eprintln!("Usage: {} <enable> <limit>", args[0]);
-    //     std::process::exit(1);
-    // }
-
-    // bot trigger
-    let _boool = args[1].parse::<bool>().expect("Err: Bad bool input");
     let boool = true;
     if !boool {
         return;
     }
-    
-    // threshold limit argument
-    let _input: u128 = args[2]
-        .trim()
-        .parse()
-        .expect("Enter an integer as argument two");
 
     let input = 100;
 
