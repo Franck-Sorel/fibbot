aa43129a649f38dd43a5cc03bbef20a60b86dff8
Franck-Sorel <mfrancksorel@gmail.com>
2025-02-28 16:19:01 +0100

    Update: remove automated pr_number

--- src/read_pull_request.rs
+++ src/read_pull_request.rs
@@ -8,27 +8,12 @@ struct PullRequest {
 
 }
 
-// fn get_pull_request_content(repo: &str, pr_number: u32) -> Result<String, reqwest::Error> {
-//     let token = env::var("GITHUB_TOKEN").expect("GITHUB_TOKEN must be set");
-//     let url = format!("https://api.github.com/repos/{}/pulls/{}", repo, pr_number);
-
-//     let client = Client::new();
-//     let response = client.get(&url)
-//         .header("Authorization", format!("token {}", token))
-//         .send()?;
-
-//     if response.status().is_success() {
-//         let pr: PullRequest = response.json()?;
-//         Ok(pr.body)
-//     } else {
-//         Err(reqwest::Error::from(response.error_for_status().unwrap_err()))
-//     }
-// }
 
 pub fn read_pull() -> Result<String, reqwest::Error> {
     let client = Client::new();
 
-    let pull_request_number = env::var("PR_NUMBER").expect("Bad pull request number").parse::<u32>().expect("Bad_parsing pull request number");
+    let pull_request_number = 1;
+
     let owner_repo = env::var("GITHUB_REPOSITORY").expect("Bad Repo");
     let pull_request_url = format!("https://api.github.com/repos/{owner_repo}/pulls/{pull_request_number}");
 
