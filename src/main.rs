ec7bae5965b1887504342496eb15b52e84bcdf29
Franck-Sorel <mfrancksorel@gmail.com>
2025-02-28 15:53:52 +0100

    Update: add custom argument

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,28 @@ use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
+
+
     // Check if the amount of arguments is exactly 3 (program name + 2 arguments)
-    if args.len() != 3 {
-        eprintln!("Usage: {} <enable> <limit>", args[0]);
-        std::process::exit(1);
-    }
+    // if args.len() != 3 {
+    //     eprintln!("Usage: {} <enable> <limit>", args[0]);
+    //     std::process::exit(1);
+    // }
 
     // bot trigger
-    let boool = args[1].parse::<bool>().expect("Err: Bad bool input");
+    let _boool = args[1].parse::<bool>().expect("Err: Bad bool input");
+    let boool = true;
     if !boool {
         return;
     }
     
     // threshold limit argument
-    let input: u128 = args[2]
+    let _input: u128 = args[2]
         .trim()
         .parse()
         .expect("Enter an integer as argument two");
+
+    let input = 100;
 
     // Pull request content as String
     let text = read_pull_request::read_pull();
