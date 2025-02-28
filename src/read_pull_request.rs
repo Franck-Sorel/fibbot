043e7dab35c1068c9a15bb21ead6f8d4907d5c30
Franck-Sorel <mfrancksorel@gmail.com>
2025-02-28 16:59:18 +0100

    update: token

--- src/read_pull_request.rs
+++ src/read_pull_request.rs
@@ -17,7 +17,14 @@ pub fn read_pull() -> Result<String, reqwest::Error> {
     let owner_repo = env::var("GITHUB_REPOSITORY").expect("Bad Repo");
     let pull_request_url = format!("https://api.github.com/repos/{owner_repo}/pulls/{pull_request_number}");
 
-    let token = env::var("GITHUB_TOKEN").expect("token_Error");
+    let token = match env::var("GH_TOKEN") {
+        Ok(token) => token,
+        Err(_) => {
+            eprintln!("GitHub token not found in environment variables.");
+            std::process::exit(1);
+        }
+        
+    };
 
     let response = client.get(&pull_request_url)
         .header("User-Agent", "foo")
