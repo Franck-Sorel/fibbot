e341434c014b0ddd13d63564165a84098715fd48
Franck-Sorel <mfrancksorel@gmail.com>
2025-03-01 09:45:09 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -14,12 +14,11 @@ async fn main() {
     let limit = 100 as u128;
 
     // Pull request content as String
-    let text = read_pull_request::read_pull();
-    let content = text.await.expect("error taking the pull request content as sting");
+    
     
     let pr_number = 1 as u128;
 
-    let _content = content.as_str();
+    
 
    let content = get_from_pull_request::get_pull_request(pr_number, limit).await;
 
